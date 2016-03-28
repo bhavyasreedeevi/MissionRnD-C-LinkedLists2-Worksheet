78ada81eae104f2d311c51a6b3357164a8542cb6
bhavyasreedeevi <bhavyasreedeevi@missionrnd.com>
2016-03-29 00:03:55 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -12,12 +12,64 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
-
+struct node *current;
+struct node *list = NULL;
+struct node* createlist(int data)
+{
+	struct node *new_node;
+	new_node = (struct node*)malloc(sizeof(struct node));
+	new_node->num = data;
+	new_node->next = NULL;
+	if (list == NULL)
+	{
+		list = new_node;
+		current = new_node;
+	}
+	else
+	{
+		current->next = new_node;
+		current = new_node;
+	}
+	return(list);
+}
 struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+	if (head1 == NULL && head2 == NULL)
+		return NULL;
+	else if (head1 != NULL&&head2 == NULL)
+		return head1;
+	else if (head2 != NULL&&head1 == NULL)
+		return head2;
+	list = NULL;
+	int temp, i, k = 0;
+	struct node *result = NULL, *temp1 = head1, *temp2 = head2;
+	while (temp1 != NULL&&temp2 != NULL)
+	{
+		if ((temp1->num)< (temp2->num))
+		{
+
+			result = createlist(temp1->num);
+			temp1 = temp1->next;
+		}
+		else
+		{
+			result = createlist(temp2->num);
+			temp2 = temp2->next;
+		}
+	}
+	while (temp1 != NULL)
+	{
+		result = createlist(temp1->num);
+		temp1 = temp1->next;
+	}
+	while (temp2 != NULL)
+	{
+		result = createlist(temp2->num);
+		temp2 = temp2->next;
+	}
+	return result;
 }
